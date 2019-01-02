65a448a746cf9f20e1aaff3f2b8e8a280d8b7839
Jeffrey Ng <ng.jeffrey@rogers.com>
2019-01-02 14:56:13 -0500

    Added js with AJAX to connect with python file

---
+++ static/js/javascript.js
@@ -0,0 +1,92 @@
+$(function() {
+    $('#A1').bind('click', function() {
+        $.getJSON('/A1',
+            function(data) {
+              //do nothing
+            });
+            return false;
+    });
+});
+
+$(function() {
+    $('#A2').bind('click', function() {
+        $.getJSON('/A2',
+            function(data) {
+              //do nothing
+            });
+            return false;
+    });
+});
+
+$(function() {
+    $('#A3').bind('click', function() {
+        $.getJSON('/A3',
+            function(data) {
+              //do nothing
+            });
+            return false;
+    });
+});
+
+$(function() {
+    $('#B1').bind('click', function() {
+        $.getJSON('/B1',
+            function(data) {
+              //do nothing
+            });
+            return false;
+    });
+});
+
+$(function() {
+    $('#B2').bind('click', function() {
+        $.getJSON('/B2',
+            function(data) {
+              //do nothing
+            });
+            return false;
+    });
+});
+
+
+
+$(function() {
+    $('#B3').bind('click', function() {
+        $.getJSON('/B3',
+            function(data) {
+              //do nothing
+            });
+            return false;
+    });
+});
+
+$(function() {
+    $('#C1').bind('click', function() {
+        $.getJSON('/C1',
+            function(data) {
+              //do nothing
+            });
+            return false;
+    });
+});
+
+$(function() {
+    $('#C2').bind('click', function() {
+        $.getJSON('/C2',
+            function(data) {
+              //do nothing
+            });
+            return false;
+    });
+});
+
+
+$(function() {
+    $('#C3').bind('click', function() {
+        $.getJSON('/C3',
+            function(data) {
+              //do nothing
+            });
+            return false;
+    });
+});
